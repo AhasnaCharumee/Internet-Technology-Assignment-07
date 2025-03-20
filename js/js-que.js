4c2aad9edd2da9e1f640e8f89f264ac092b22acc
AhasnaCharumee <charumeeahasna@gmail.com>
2025-03-20 17:20:24 +0530

    JS QUE 1 Done

---
+++ js/js-que.js
@@ -0,0 +1,37 @@
+console.log("queue-1.js file is loaded");
+
+let letters = ["A", "B", "C", "D", "E", "F"];
+
+let persons = new Array(6);
+for (let i = 0; i < 6; i++) {
+  persons[i] = document.getElementById("per-" + (i + 1));
+}
+
+let currentPer = 0;
+let currentLetter = 5;
+
+function startLoop() {
+  for (let i = 0; i < persons.length; i++) {
+    document.getElementById("per-" + (currentPer + 1)).innerHTML =
+      '<span class="letter">' + letters[currentLetter] + "</span>";
+
+    console.log("per - " + (currentPer + 1) + " and letter - " + currentLetter);
+    calculateCurrentLetter();
+    calculateCurrentPer();
+  }
+  changeLetter();
+}
+
+function calculateCurrentLetter() {
+  currentLetter = (currentLetter + 1) % letters.length;
+}
+
+function calculateCurrentPer() {
+  currentPer = (currentPer + 1) % persons.length;
+}
+
+function changeLetter() {
+  currentLetter = (currentLetter + letters.length - 1) % letters.length;
+}
+
+setInterval(startLoop, 1000);
