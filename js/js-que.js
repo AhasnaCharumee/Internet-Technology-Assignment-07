7d5cbb85a647587b5d472a2e2d1a0502824ef3b1
AhasnaCharumee <charumeeahasna@gmail.com>
2025-03-22 08:11:16 +0530

    aded start stop btn to js que

--- js/js-que.js
+++ js/js-que.js
@@ -10,6 +10,8 @@ for (let i = 0; i < 6; i++) {
 let currentPer = 0;
 let currentLetter = 5;
 
+let intervalId = null;
+
 function startLoop() {
   for (let i = 0; i < persons.length; i++) {
     document.getElementById("per-" + (currentPer + 1)).innerHTML =
@@ -20,6 +22,20 @@ function startLoop() {
     calculateCurrentPer();
   }
   changeLetter();
+}
+
+// Control functions
+function startAnimation() {
+  if (!intervalId) {
+    intervalId = setInterval(startLoop, 1000);
+  }
+}
+
+function stopAnimation() {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
 }
 
 function calculateCurrentLetter() {
@@ -34,4 +50,6 @@ function changeLetter() {
   currentLetter = (currentLetter + letters.length - 1) % letters.length;
 }
 
-setInterval(startLoop, 1000);
+// Add button listeners
+document.getElementById("start-btn").addEventListener("click", startAnimation);
+document.getElementById("stop-btn").addEventListener("click", stopAnimation);
